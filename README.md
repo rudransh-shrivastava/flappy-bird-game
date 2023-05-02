9cc42eb9559ac43c4f8493358154f49ccf4b2c99
Rudransh Shrivastava <rudransh.shr@gmail.com>
2023-05-02 11:12:53 +0530

    Removed Circle and added birdimage BufferImage

--- README.md
+++ README.md
@@ -3,10 +3,8 @@ Flappy Bird Game Clone using Swing in Java <br>
 *** TODO LIST *** <br>
 <br>
 
---tweak gravity, jump and pipe velocity values <br>
---add multiple pipes using ArrayLists (probably the hardest part) <br>
 --display score <br>
 --change Rectangles to be an image (I wasnt able to do this before so I'll do it in the end) <br>
 --publish the game on replit so anyone can play it! <br>
-
+--add animations for collision and bird movements <br>
 
--- src/com/rudransh/Bird.java
+++ src/com/rudransh/Bird.java
@@ -2,16 +2,30 @@ package com.rudransh;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Bird extends Rectangle {
     //declaring values
     float yVelocity = -3;
-    float gravity = 0.4F; // todo this gravity is like standing on the sun
+    float gravity = 0.4F;
+    BufferedImage birdImage;
+
+
 
     Bird(float x, float y, float width, float height){
         super((int)x, (int)y, (int)width, (int)height);
+
+        try {
+            birdImage = ImageIO.read(new File("res/bird.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // this code runs every tick
@@ -20,8 +34,7 @@ public class Bird extends Rectangle {
         yVelocity += gravity;
     }
     public void draw(Graphics g) {
-        g.setColor(Color.white);
-        g.fillOval(x, y, width, height);
+        g.drawImage(birdImage, x, y, null);
     }
 
 }
